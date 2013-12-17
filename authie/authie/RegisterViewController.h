b9032d630f2ca89f334d66593e7f705de5c1c8be
Seth Hayward <seth.hayward@gmail.com>
2013-12-17 07:44:53 -0800

    setting up an initial screen for registering a handle

---
+++ authie/authie/RegisterViewController.h
@@ -0,0 +1,13 @@
+//
+//  RegisterViewController.h
+//  authie
+//
+//  Created by Seth Hayward on 12/16/13.
+//  Copyright (c) 2013 bitwise. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RegisterViewController : UIViewController
+
+@end
