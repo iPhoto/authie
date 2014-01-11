8ce7522934d3b5a3bc71182ee438d1bb7dc22bb4
Seth Hayward <seth.hayward@gmail.com>
2014-01-10 19:21:45 -0800

    minithread vc will hold one snap that is on the users public profile

---
+++ authie/authie/MiniThreadViewController.h
@@ -0,0 +1,14 @@
+//
+//  MiniThreadViewController.h
+//  authie
+//
+//  Created by Seth Hayward on 1/10/14.
+//  Copyright (c) 2014 bitwise. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MiniThreadViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIImageView *snapView;
+
+@end
