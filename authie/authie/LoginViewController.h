ecf0b0b568a6acbb080a374b2091e58543f97fbf
Seth Hayward <seth.hayward@gmail.com>
2014-01-11 19:15:00 -0800

    drew login screen

---
+++ authie/authie/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  authie
+//
+//  Created by Seth Hayward on 1/11/14.
+//  Copyright (c) 2014 bitwise. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
