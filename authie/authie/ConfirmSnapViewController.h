8d04c2a7bb410c3fa8f3720ea0675b2a3058c845
Seth Hayward <seth.hayward@gmail.com>
2014-01-13 13:14:33 -0800

    confirm snap view start

---
+++ authie/authie/ConfirmSnapViewController.h
@@ -0,0 +1,13 @@
+//
+//  ConfirmSnapViewController.h
+//  authie
+//
+//  Created by Seth Hayward on 1/13/14.
+//  Copyright (c) 2014 bitwise. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ConfirmSnapViewController : UIViewController
+
+@end
