d98fdf87d74ba25c54aa048a8e942d109b81988d
Seth Hayward <seth.hayward@gmail.com>
2014-01-17 11:13:37 -0800

    renable test flight

--- authie/authie/AppDelegate.m
+++ authie/authie/AppDelegate.m
@@ -48,7 +48,7 @@
     [_window setTintColor:[UIColor blackColor]];
     
     // test flighty
-    //[TestFlight takeOff:@"e0b1dd9c-b710-4223-8aa8-b68350a2da33"];
+    [TestFlight takeOff:@"e0b1dd9c-b710-4223-8aa8-b68350a2da33"];
 
     // urchin
 
