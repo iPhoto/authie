25d39fb671f3e218ec58c8089f21bba627f83a9a
Seth Hayward <seth.hayward@gmail.com>
2014-01-21 08:47:12 -0800

    encode/decode so i can save received messages

--- authie/authie/RODMessage.m
+++ authie/authie/RODMessage.m
@@ -9,5 +9,37 @@
 #import "RODMessage.h"
 
 @implementation RODMessage
+@synthesize active, anon, fromHandle, id, messageText, thread, toHandleSeen, sentDate;
+
+- (void) encodeWithCoder:(NSCoder *)aCoder
+{
+    
+    [aCoder encodeObject:self.id forKey:@"id"];
+    [aCoder encodeObject:self.fromHandle forKey:@"fromHandle"];
+    [aCoder encodeObject:self.thread forKey:@"thread"];
+    [aCoder encodeObject:self.sentDate forKey:@"sentDate"];
+    [aCoder encodeObject:self.active forKey:@"active"];
+    [aCoder encodeObject:self.anon forKey:@"anon"];
+    [aCoder encodeObject:self.toHandleSeen forKey:@"toHandleSeen"];
+    [aCoder encodeObject:self.messageText forKey:@"messageText"];
+    
+}
+
+- (id) initWithCoder:(NSCoder *)aDecoder
+{
+    self = [super init];
+    if (self) {
+        [self setId:[NSNumber numberWithInteger:[[aDecoder decodeObjectForKey:@"id"] integerValue]]];
+        [self setFromHandle:[aDecoder decodeObjectForKey:@"fromHandle"]];
+        [self setThread:[aDecoder decodeObjectForKey:@"thread"]];
+        [self setSentDate:[aDecoder decodeObjectForKey:@"sentDate"]];
+        [self setActive:[NSNumber numberWithInteger:[[aDecoder decodeObjectForKey:@"active"] integerValue]]];
+        [self setAnon:[NSNumber numberWithInteger:[[aDecoder decodeObjectForKey:@"anon"] integerValue]]];
+        [self setToHandleSeen:[NSNumber numberWithInteger:[[aDecoder decodeObjectForKey:@"toHandleSeen"] integerValue]]];
+        [self setMessageText:[aDecoder decodeObjectForKey:@"messageText"]];        
+    }
+    return self;
+}
+
 
 @end
