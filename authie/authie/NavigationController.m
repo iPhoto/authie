f27d8adcdf7d8b9e399d28411853eacb8d9d4931
Seth Hayward <seth.hayward@gmail.com>
2014-01-29 09:36:09 -0800

    cleanup

--- authie/authie/NavigationController.m
+++ authie/authie/NavigationController.m
@@ -24,11 +24,8 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-
-    [self.view setBackgroundColor:[UIColor whiteColor]];
-    [self.view addGestureRecognizer:[[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(panGestureRecognized:)]];
- 
     
+    [self.view addGestureRecognizer:[[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(panGestureRecognized:)]];
 }
 
 - (void)didReceiveMemoryWarning
@@ -46,44 +43,6 @@
 {
 // no more pan gesture, this is annoying
 //    [self.frostedViewController panGestureRecognized:sender];
-    
-}
-
-
-- (void)addMessage:(NSString *)user message:(NSString *)msg groupKey:(NSString *)key
-{
-    NSLog(@"addMessage, nav: %@, %@, %@, %i", user, msg, key, [RODItemStore sharedStore].hubConnection.state);
-    [[RODItemStore sharedStore] addChat:user message:msg groupKey:key];
-    
-    //NSString *s = [NSString stringWithFormat:@"%@ said: %@", user, msg];
-    // Print the message when it comes in
-    
-    //UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"new auth" message:s delegate:self cancelButtonTitle:@"ok" otherButtonTitles:@"go to thread", nil];
-    //[alert show];
-    //self.mostRecentGroupKey = key;
-    
-    //
-    //
-    //    if([dashViewController.navigationController.topViewController class] != [ThreadViewController class]) {
-    //        // looking at dash, invite, private key, compose
-    //        self.mostRecentGroupKey = key;
-    //        [alert show];
-    //    } else {
-    //
-    //        // looking at a thread, may be the one we need to update
-    //
-    //        NSLog(@"Toplevel was a threadviewcontroller.");
-    //        NSString *current_group_key = threadViewController.thread.groupKey;
-    //
-    //        if([current_group_key isEqualToString:key]) {
-    //            // reload the current messages..??????
-    //        } else {
-    //            self.mostRecentGroupKey = key;
-    //        }
-    //
-    //        [alert show];
-    //
-    //    }
     
 }
 
