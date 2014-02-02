9f9d3a1da47cafb32a04dcc2b4fbedee3895a1f0
Seth Hayward <seth.hayward@gmail.com>
2014-02-02 10:25:08 -0800

    adding the beginning of the pager view controller for the dash

---
+++ authie/authie/PagerViewController.h
@@ -0,0 +1,13 @@
+//
+//  PagerViewController.h
+//  authie
+//
+//  Created by Seth Hayward on 2/2/14.
+//  Copyright (c) 2014 bitwise. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PagerViewController : UIViewController
+
+@end
---
+++ authie/authie/PagerViewController.m
@@ -0,0 +1,38 @@
+//
+//  PagerViewController.m
+//  authie
+//
+//  Created by Seth Hayward on 2/2/14.
+//  Copyright (c) 2014 bitwise. All rights reserved.
+//
+
+#import "PagerViewController.h"
+
+@interface PagerViewController ()
+
+@end
+
+@implementation PagerViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
