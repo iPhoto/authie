f8d7388dd533071cca1b798bb5d194576e2741f7
Seth Hayward <seth.hayward@gmail.com>
2014-02-09 01:33:50 -0800

    trying out a darker nav

--- authie/authie/NavigationViewController.m
+++ authie/authie/NavigationViewController.m
@@ -29,7 +29,7 @@
 {
     [super viewDidLoad];
     if (REUIKitIsFlatMode()) {
-        [self.navigationBar performSelector:@selector(setBarTintColor:) withObject:[UIColor colorWithRed:0/255.0 green:213/255.0 blue:161/255.0 alpha:1]];
+        [self.navigationBar performSelector:@selector(setBarTintColor:) withObject:[UIColor colorWithRed:0/255.0 green:60/255.0 blue:45/255.0 alpha:1]];
         self.navigationBar.tintColor = [UIColor whiteColor];
     } else {
         self.navigationBar.tintColor = [UIColor colorWithRed:0 green:196/255.0 blue:147/255.0 alpha:1];
